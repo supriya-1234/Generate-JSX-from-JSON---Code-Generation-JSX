11c91a1a84fa94b21aa40438c15b923da74822d4
supriya-1234 <66170913+supriya-1234@users.noreply.github.com>
2020-10-21 19:49:27 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,10 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
+  render() {
+    return <></>;
+  }
 }
-
 
 export default App;
